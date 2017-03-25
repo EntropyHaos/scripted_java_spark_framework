8d04c7489941ced4100050cc9a1789c595afe935
KoreaHaos <koreahaos@gmail.com>
2017-03-25 06:28:43 +0000

    Remove junk.

--- z_haos/setup_c9_vm_for_java_spark.bash
+++ z_haos/setup_c9_vm_for_java_spark.bash
@@ -1,20 +1,28 @@
-# TODO : Make it record times and other stuff(s)?
+# This a simple script used to setup a Cloud9 VM.
 
-function init()
-{
-    echo_action "Setup Configs for Silent Java Install" true
-    setup_configs_for_silent_java_sdk_install
-    echo_action "Setup Spark Framework on Cloud9" true
-    setup_cloud9_vm_for_spark_framework
+# ToDo add function to delete script after its been run.
+
+function init() {
+    get_confirmation;
+    setup_cloud9_vm_for_spark_framework;
 }
 
-function setup_configs_for_silent_java_sdk_install(){
-    echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-    echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+function get_confirmation(){
+    clear
+    echo_action "You are about to install Java8 and Maven on this machine." true
+    read -p "Do you want to do this? " -n 1 -r
+    echo    # move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        echo_action "Installation will start in 2 seconds." true
+    else
+        exit;
+    fi
+    sleep 2  # Waits 2 seconds.
+    clear
 }
 
-function setup_cloud9_vm_for_spark_framework()
-{
+function setup_cloud9_vm_for_spark_framework() {
     echo_action "Adding external APT repository." true
     sudo add-apt-repository ppa:webupd8team/java -y
     echo_action "Downloading package lists from the repositories and updating them." true
@@ -25,19 +33,15 @@ function setup_cloud9_vm_for_spark_framework()
     sudo apt-get install oracle-java8-set-default -y
     echo_action "Installing Maven" true
     sudo apt-get install maven -y
-    : << 'EOP'
-EOP
 }
 
-function create_line_across_terminal()
-{
+function create_line_across_terminal() {
     echo
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     echo
 }
 
-function echo_action()
-{
+function echo_action() {
     if [ "$2" = true ]
     then
         create_line_across_terminal
@@ -50,41 +54,12 @@ function echo_action()
     fi
 }
 
-function run_test()
-{
-    echo "test completed."
-}
-
-function run_silent_install_of_framework(){
-    # It's so easy...
-    log_dir="log_haos"
-    # Move to C9 project root.
-    cd $GOPATH
-    # Go one directory up.
-    cd ..
-    # Make the logfile directory if it is not there.
-    mkdir -p $log_dir
-    # Drop down into it.
-    cd $log_dir
-    # Set the name for the log file.
-    log_file="spark_framework_install.txt"
-    # Run the setup script and append write to log file.
-    
-    # This runs the setup with no output to console logging to file.
-    #init >> "$log_file" 2>&1
-    # This works for logging and viewing install at the same time.
-    init | tee -a "$log_file"
-    
-    # Go back to the project's root directory.
-    cd $GOPATH
-}
-
 function delete_this_script() {
     directory_script_is_run_from="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     script_name=`basename "$0"`
     cd $directory_script_is_run_from
     rm $script_name
 }
-# run_silent_install_of_framework
 
-delete_this_script;
+init
+delete_this_script
